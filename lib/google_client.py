4ded9d6b45045e7617c7b59e4b17ce1b98322c38
MENOURY Sebastien (SNCF VOYAGEURS / DIRECTION GENERALE TER / ter siv ddti dv dist com) <7311576R@COMMUN.AD.SNCF.FR>
2025-05-09 14:35:47 +0200

    ✅ Corrige le chemin du fichier JSON pour Render

--- lib/google_client.py
+++ lib/google_client.py
@@ -1,22 +1,18 @@
-import os
 import gspread
+import os
 from oauth2client.service_account import ServiceAccountCredentials
 
-# Connexion au client Google Sheets via la clé de service
 def get_client():
     scope = [
         "https://spreadsheets.google.com/feeds",
         "https://www.googleapis.com/auth/drive"
     ]
-
-    # Chemin absolu vers le fichier JSON, même si le script est lancé depuis ailleurs
+    # Chemin vers le fichier JSON dans le dossier credentials
     key_path = os.path.join(os.path.dirname(__file__), "credentials", "greenway-459306-9a34d3da67f4.json")
-
     creds = ServiceAccountCredentials.from_json_keyfile_name(key_path, scope)
     return gspread.authorize(creds)
 
-# Récupère un onglet par son nom (ex : "Ref-Usages")
-def get_worksheet(nom_onglet):
+def get_worksheet(sheet_name):
     client = get_client()
-    sheet = client.open("Biocarbon Données")  # nom exact du Google Sheet
-    return sheet.worksheet(nom_onglet)
+    sheet = client.open("BioCarbon Données")
+    return sheet.worksheet(sheet_name)
