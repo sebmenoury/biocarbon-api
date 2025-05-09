71bb0bd67bf4203b2f3a5c6c713dd2a9fa7257a1
MENOURY Sebastien (SNCF VOYAGEURS / DIRECTION GENERALE TER / ter siv ddti dv dist com) <7311576R@COMMUN.AD.SNCF.FR>
2025-05-09 15:01:10 +0200

    🔐 Utilise le secret Render pour l'auth Google

--- lib/google_client.py
+++ lib/google_client.py
@@ -1,5 +1,4 @@
 import gspread
-import os
 from oauth2client.service_account import ServiceAccountCredentials
 
 def get_client():
@@ -7,12 +6,11 @@ def get_client():
         "https://spreadsheets.google.com/feeds",
         "https://www.googleapis.com/auth/drive"
     ]
-    # Chemin vers le fichier JSON dans le dossier credentials
-    key_path = os.path.join(os.path.dirname(__file__), "credentials", "greenway-459306-9a34d3da67f4.json")
+    key_path = "/etc/secrets/greenway_459306.json"
     creds = ServiceAccountCredentials.from_json_keyfile_name(key_path, scope)
     return gspread.authorize(creds)
 
 def get_worksheet(sheet_name):
     client = get_client()
-    sheet = client.open("BioCarbon Données")
-    return sheet.worksheet(sheet_name)
+    sheet = client.open(sheet_name).sheet1
+    return sheet
