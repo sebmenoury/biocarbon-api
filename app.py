36b446fa3445702d6cb4a741bf41fb4d7c29a770
MENOURY Sebastien (SNCF VOYAGEURS / DIRECTION GENERALE TER / ter siv ddti dv dist com) <7311576R@COMMUN.AD.SNCF.FR>
2025-05-10 17:04:02 +0200

    ✅ APassage en Poste

--- app.py
+++ app.py
@@ -10,10 +10,9 @@ from lib.api.api_ref_alimentation import bp_ref_alimentation
 from lib.api.api_ref_aeroports import bp_ref_aeroports
 from lib.api.api_ref_equipements import bp_ref_equipements
 
-from lib.api.api_UC_usages import bp_uc_usages
+from lib.api.api_UC_postes import bp_uc_postes
 from lib.api.api_UC_individu import bp_uc_individu
 from lib.api.api_UC_synthese import bp_uc_synthese
-from lib.api.api_UC_equipements import bp_uc_equipements
 
 
 app = Flask(__name__)
@@ -29,8 +28,7 @@ app.register_blueprint(bp_ref_alimentation)
 app.register_blueprint(bp_ref_aeroports)
 app.register_blueprint(bp_ref_equipements)
 
-app.register_blueprint(bp_uc_equipements)
-app.register_blueprint(bp_uc_usages)
+app.register_blueprint(bp_uc_postes)
 app.register_blueprint(bp_uc_individu)
 app.register_blueprint(bp_uc_synthese)
 
