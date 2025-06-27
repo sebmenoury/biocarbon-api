b4faa429143807ff327db766c508f773134b74c4
MENOURY Sebastien (SNCF VOYAGEURS / DIRECTION GENERALE TER / ter siv ddti dv dist com) <7311576R@COMMUN.AD.SNCF.FR>
2025-06-27 17:38:10 +0200

    ✅ Ajout champ Type_Objet dans UC-Usages et mise à jour API

--- sheets_service.py
+++ sheets_service.py
@@ -1,5 +1,19 @@
 import gspread
 
-gc = gspread.service_account(filename='credentials.json')  # ou autre méthode selon ton setup
-sh = gc.open("Biocarbon Données")  # adapte le nom ici
+import os
+import json
+import gspread
+from google.oauth2.service_account import Credentials
+
+# Charger les credentials depuis une variable d'environnement
+service_account_info = json.loads(os.environ["GOOGLE_CREDENTIALS_JSON"])
+
+# Créer le client gspread
+creds = Credentials.from_service_account_info(service_account_info, scopes=[
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive"
+])
+gc = gspread.authorize(creds)
+
+sh = gc.open("Biocarbon Données") 
 sheet_uc_postes = sh.worksheet("UC-Postes")
