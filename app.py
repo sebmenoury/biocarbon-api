0b805cb6e8ec1b6a0f76bf2d856605ccf8b6d472
MENOURY Sebastien (SNCF VOYAGEURS / DIRECTION GENERALE TER / ter siv ddti dv dist com) <7311576R@COMMUN.AD.SNCF.FR>
2025-06-27 18:33:16 +0200

    ✅ Ajout champ Type_Objet dans UC-Usages et mise à jour API

--- app.py
+++ app.py
@@ -34,10 +34,10 @@ app.register_blueprint(bp_ref_aeroports)
 app.register_blueprint(bp_ref_equipements)
 app.register_blueprint(bp_ref_type_categories)
 
-app.register_blueprint(bp_uc_postes, url_prefix='/api/uc')
-app.register_blueprint(bp_uc_biens, url_prefix='/api/uc')
+app.register_blueprint(bp_uc_postes, url_prefix='/api/uc/postes')
+app.register_blueprint(bp_uc_biens, url_prefix='/api/uc/biens')
 app.register_blueprint(bp_uc_individu)
-app.register_blueprint(bp_uc_synthese, url_prefix='/api/uc')
+app.register_blueprint(bp_uc_synthese)
 
 if __name__ == "__main__":
     print("✅ ROUTES DISPONIBLES :")
